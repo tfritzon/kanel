5d29b230a0433e914b0757bd49aba143537c5472
Thorbiorn Fritzon <tfritzon@spotify.com>
2016-04-23 01:22:45 +0200

    Second test: using Unsafe

--- tests/SimpleArray.java
+++ tests/SimpleArray.java
@@ -1,12 +1,14 @@
 import java.util.UUID;
+import sun.misc.Unsafe;
+import java.lang.reflect.Field;
 
 class UUIDString {
-    Object uuid;
+    long uuid;
 
-    public native Object alloc(int n);
+    //public native Object alloc(int n);
     
-    public UUIDString(String s) {
-	this.uuid = alloc(s.length());
+    public UUIDString(Unsafe u, String s) {
+	this.uuid = u.allocateMemory(s.length());
 	System.out.println(s.length() + ": " + this.uuid);
     }
 }
@@ -15,13 +17,26 @@ public class SimpleArray {
     static UUIDString[] a = new UUIDString[10];
 
     public static void main(String[] argv) {
+	Unsafe u = getUnsafe();
 	for(int i= 0; i < 10; i++) {
-	    a[i] = new UUIDString(UUID.randomUUID().toString());
+	    a[i] = new UUIDString(u, UUID.randomUUID().toString());
 	}
     }
 
+    /*
     static {
 	System.loadLibrary("UUIDString");
+    }
+    */
+    public static Unsafe getUnsafe() {
+	try {
+	    Field f = Unsafe.class.getDeclaredField("theUnsafe");
+	    f.setAccessible(true);
+	    return (Unsafe)f.get(null);
+	} catch (Exception e) { 
+	    e.printStackTrace();
+	}
+	return null;
     }
 }
 
